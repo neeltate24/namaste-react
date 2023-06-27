26281816b03c93bf5e22bf1dc4416378dc537e99
Neel <neel.tate24@gmail.com>
2023-06-27 19:23:35 +0530

    01 inception complete with assignments on 27 june 2023

---
+++ inception/react-in-html/App.js
@@ -0,0 +1,38 @@
+/**
+ *
+ *<div id="parent">
+ *   <div id="child">
+ *      <h1>I'm h1 tag</h1>
+ *      <h2>I'm h2 tag</h2>
+ *   </div>
+ *   <div id="child2">
+ *      <h1>I'm h1 tag</h1>
+ *      <h2>I'm h2 tag</h2>
+ *   </div>
+ *</div>
+ *
+ * ReactElement(Object) => HTML(Browser Understands)
+ */
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React!"
+// );
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
+
+console.log(parent); //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
